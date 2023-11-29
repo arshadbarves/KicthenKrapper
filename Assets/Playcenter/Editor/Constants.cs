f420168ca33e338cbb219b5fb6de9b02d094f3a8
Arshad Barves <arshadbarves@gmail.com>
2023-11-29 18:05:53 +0530

    Fixed EOS issue

---
+++ Assets/Playcenter/Editor/Constants.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayCenter.Editor
+{
+    public abstract class Constants
+    {
+        public const string Menu = "Playcenter/";
+    }   
+}
